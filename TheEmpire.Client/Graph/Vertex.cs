c78147739b02f5b97bf0bbb28a212b12753d645c
Gustas Sagaitis <gustassagaitis@gmail.com>
2016-11-12 14:26:36 +0200

    graph base

---
+++ TheEmpire.Client/Graph/Vertex.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections.Generic;
+
+namespace TheEmpire.Client.Graph
+{
+    public partial class Vertex
+    {
+        public Vertex(int x, int y, bool hasCookie, bool hasGhost, bool hasTacman)
+        {
+            X = x;
+            Y = y;
+            HasGhost = hasGhost;
+            HasTacman = hasTacman;
+            HasCookie = hasCookie;
+            Edges = new List<Edge>();
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool HasGhost { get; set; }
+        public bool HasTacman { get; set; }
+        public bool HasCookie { get; set; }
+
+        public List<Edge> Edges { get; private set; }
+    }
+}
