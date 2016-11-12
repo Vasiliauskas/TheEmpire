b96a2efa70f78fb2a3a1d2e7502660588fe7423d
Gustas Sagaitis <gustassagaitis@gmail.com>
2016-11-12 10:16:01 +0200

    add authentication code calc

--- TheEmpire.Client/DTO/ReqAuth.cs
+++ TheEmpire.Client/DTO/ReqAuth.cs
@@ -1,17 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TheEmpire.Client.DTO
 {
-    class ReqAuth
+    //AuthString := TeamName + ‘:’ + ClientName + ‘:’ + SessionId + ‘:’ +
+    //SequenceNumber
+    //AuthCode := GetAuthCode(AuthString)
+    //GetAuthCode(Payload):
+    //SignBytes := GetUTF8Bytes(Payload + Secret)
+    //Return LowercaseHex(SHA1(SignBytes))
+    public class ReqAuth
     {
-        string TeamName;
-        string ClientName;
-        int SessionId;
-        int SequenceNumber;
-        string AuthCode;
+        string TeamName { get; set; }
+        string ClientName { get; set; }
+        int SessionId { get; set; }
+        int SequenceNumber { get; set; }
+        string AuthCode
+        {
+            get
+            {
+                var secret = ConfigurationManager.AppSettings["AuthSecret"];
+                var authString = $"{TeamName}:{ClientName}:{SessionId}+{SequenceNumber}{secret}";
+                return ComputeSha1(authString);
+            }
+        }
+
+        private static string ComputeSha1(string input)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var sb = new StringBuilder(hash.Length * 2);
+                foreach (byte b in hash)
+                {
+                    // can be "x2" if you want lowercase
+                    sb.Append(b.ToString("x2"));
+                }
+                return sb.ToString();
+            }
+        }
     }
 }
