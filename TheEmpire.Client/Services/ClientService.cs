4733f9d2df9ef320fce54e34ca762e9d6ddecac5
Vasiliauskas <vidasvasiliauskas@gmail.com>
2016-11-12 09:42:43 +0200

    service

---
+++ TheEmpire.Client/Services/ClientService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheEmpire.Client
+{
+    class ClientService
+    {
+        public bool CreatePlayer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool WaitNextTurn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public GetPlayerView()
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public bool PerformMove()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
