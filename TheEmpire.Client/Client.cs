d183939b10473b6d9ded1c064b1d6fef874c83cd
Vasiliauskas <vidasvasiliauskas@gmail.com>
2016-11-12 12:12:53 +0200

    Stuff 2

--- TheEmpire.Client/Client.cs
+++ TheEmpire.Client/Client.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using TheEmpire.Client.DTO;
+using TheEmpire.Client.Ghosts;
 using TheEmpire.Client.Services;
 
 namespace TheEmpire.Client
@@ -18,26 +19,6 @@ namespace TheEmpire.Client
         public Client(string serverUrl)
         {
             _service = new GameService(serverUrl);
-=======
-namespace TheEmpire.Client
-{
-    abstract class Client
-    {
-        protected readonly string _serverUrl;
-        private GhostClient _ghostClient = new GhostClient();
-        private TacManClient _tacManClient = new TacManClient();
-        protected ClientService _service;
-        protected bool _isGameComplete;
-        protected readonly GameService _service;
-
-        public Client(string serverUrl)
-        {
-            _service = new GameService(serverUrl);
-            _serverUrl = serverUrl;
-            _service = new ClientService(serverUrl);
-            _ghostClient = new GhostClient();
-            _ghostClient = new GhostClient();
->>>>>>> e71f7621b5666e47f8dcca2d5445609684725bc4
         }
 
         public void Start()
@@ -46,8 +27,6 @@ namespace TheEmpire.Client
             GetRefTurn();
             while (true)
             {
-                GetPlayerView(GetPlayerViewReq request);
-
                 var nextTurn = WaitNextTurn();
                 if (nextTurn.GameFinished)
                     return;
@@ -55,14 +34,14 @@ namespace TheEmpire.Client
                 if (nextTurn.YourTurn && !nextTurn.TurnComplete)
                 {
                     if (!TakeTurn()) // failover if one failed, do second
-                    TakeTurn();
+                        TakeTurn();
                 }
 
                 Thread.Sleep(50);
             }
         }
 
-        private WaitNextTurnResp WaitNextTurn(WaitNextTurnReq request)
+        private WaitNextTurnResp WaitNextTurn()
         {
             return _service.WaitNextTurn();
         }
@@ -83,20 +62,20 @@ namespace TheEmpire.Client
 
         protected bool TakeTurn()
         {
-            var view = _service.GetPlayerView(1);
+            var view = _service.GetPlayerView();
             try
             {
                 IEnumerable<Position> req = null;
                 if (view.Mode == "TacMan")
                     req = new TacManClient().PerformMove(view);
-                //else
-                //    new GhostClient();
+                else
+                    req = new GhostClient().PerformMove(view);
 
                 var responseMove = _service.PerformMove(req);
-                // pratesti
+                // pratesti algo
             }
-            catch(Exception ex)
-        {
+            catch (Exception ex)
+            {
                 return false;
             }
             return true;
---
+++ TheEmpire.Client/Ghosts/GhostsClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheEmpire.Client.DTO;
+using TheEmpire.Client.MapModels;
+
+namespace TheEmpire.Client.Ghosts
+{
+    class GhostClient
+    {
+        public List<Position> PerformMove(GetPlayerViewResp view)
+        {
+            var map = new Map(view.Map);
+            var cell = map.Cells.Where(c => c.Content == Content.Pacman).Single();
+            var targetCell = cell.Neighbours.First();
+
+            var response = new List<Position>();
+            response.Add(new Position() { Col = targetCell.Point.X, Row = targetCell.Point.Y });
+
+            return response;
+        }
+    }
+}
