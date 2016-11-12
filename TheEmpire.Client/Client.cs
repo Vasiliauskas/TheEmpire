3c845d05ec827d0770b40c9697e3d6076bc20f46
bbox <karolis.kajenas1@gmail.com>
2016-11-12 11:15:33 +0200

    game loop

--- TheEmpire.Client/Client.cs
+++ TheEmpire.Client/Client.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TheEmpire.Client
@@ -9,9 +10,50 @@ namespace TheEmpire.Client
     class Client
     {
         private readonly string _serverUrl;
+        private bool _isGameComplete;
         public Client(string serverUrl)
         {
             _serverUrl = serverUrl;
+        }
+
+        public void Start()
+        {
+            GetSessionID();
+            CreatePlayer();
+            GetRefTurn();
+            while (!_isGameComplete)
+            {
+                WaitNextTurn();
+                Thread.Sleep(5);
+            }
+        }
+
+        private void WaitNextTurn()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void GetRefTurn()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CreatePlayer()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void GetSessionID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void IsGameCompleted()
+        {
+        }
+
+        public virtual void TakeTurn()
+        {
         }
     }
 }
